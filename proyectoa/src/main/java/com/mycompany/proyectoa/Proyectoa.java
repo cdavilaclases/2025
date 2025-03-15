a2336c4eb57b42b579a252174ae138e97292b7bc
carlos <carlos@carlos-VirtualBox>
2025-03-15 14:26:27 -0600

    Ejemplo Login

---
+++ proyectoa/src/main/java/com/mycompany/proyectoa/Proyectoa.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoa;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author carlos
+ */
+public class Proyectoa {
+
+    
+    public static ArrayList<Usuario> usuarios = new ArrayList<>();
+    
+    public static void main(String[] args) {
+     
+        Login l = new Login();
+        l.setVisible(true);
+        
+    }
+}
