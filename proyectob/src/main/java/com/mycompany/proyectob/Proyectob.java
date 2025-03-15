fbdcd161bec2a6e091ee9fb50041deae5b671fa6
carlos <carlos@carlos-VirtualBox>
2025-03-15 14:26:43 -0600

    Ejemplo Login

---
+++ proyectob/src/main/java/com/mycompany/proyectob/Proyectob.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectob;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author carlos
+ */
+public class Proyectob {
+    
+    public static ArrayList<Usuario> usuarios = new ArrayList<>();
+
+    public static void main(String[] args) {
+        
+        Login v = new Login();
+        v.setVisible(true);
+        
+    }
+}
---
+++ proyectob/src/main/java/com/mycompany/proyectob/Usuario.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectob;
+
+/**
+ *
+ * @author carlos
+ */
+public class Usuario {
+    
+    public String usuario;
+    public String nombre;
+    public int rol;
+    public String password;
+    
+}
