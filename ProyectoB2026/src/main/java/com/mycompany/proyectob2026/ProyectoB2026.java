17f7f0f3a1d98609243603918c1f68d1cec8b902
carlos <carlos@carlos-VirtualBox>
2026-03-24 09:33:05 -0600

    Ejemplo login seccion B

---
+++ ProyectoB2026/src/main/java/com/mycompany/proyectob2026/ProyectoB2026.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectob2026;
+import java.util.ArrayList;
+/**
+ *
+ * @author carlos
+ */
+public class ProyectoB2026 {
+
+    public static ArrayList<Usuario> usuarios = new ArrayList<>();
+    public static Usuario usuarioLogin;
+    
+    public static void main(String[] args) {
+        Login l = new Login();
+        l.setVisible(true);
+        
+        Usuario u = new Usuario();
+        u.usuario = "admin";
+        u.password = "admin";
+        
+        usuarios.add(u);
+        
+    }
+}
